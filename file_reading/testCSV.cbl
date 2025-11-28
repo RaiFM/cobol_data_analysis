b7aa8bae5c85131c88881d22aa0c083014fcc436
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-11-27 23:16:40 -0300

    update csv read

--- file_reading/testCSV.cbl
+++ file_reading/testCSV.cbl
@@ -11,29 +11,31 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT ARQ-CSV
-            ASSIGN TO "C:\Users\Direnzi\Downloads\Features data set.csv"
-            ORGANIZATION IS LINE SEQUENTIAL.
+               ASSIGN TO "C:\Users\raife\Downloads\sales_data_t.csv"
+               ORGANIZATION IS LINE SEQUENTIAL.
+
+           SELECT ARQ-REL
+               ASSIGN TO "C:\Users\raife\Downloads\relatorio_vendas.txt"
+               ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
        FD ARQ-CSV.
        01 LINHA-CSV        PIC X(300).
 
+       FD ARQ-REL.
+       01 LINHA-REL        PIC X(300).
+
        WORKING-STORAGE SECTION.
        01 EOF-FLAG         PIC X VALUE "N".
 
        01 F-STORE          PIC X(20).
+       01 F-DEPT           PIC X(20).
        01 F-DATE           PIC X(20).
-       01 F-TEMPERATURE    PIC X(20).
-       01 F-FUELPRICE      PIC X(20).
-       01 F-MD1            PIC X(20).
-       01 F-MD2            PIC X(20).
-       01 F-MD3            PIC X(20).
-       01 F-MD4            PIC X(20).
-       01 F-MD5            PIC X(20).
-       01 F-CPI            PIC X(20).
-       01 F-UNEMP          PIC X(20).
+       01 F-W_SALES        PIC X(20).
        01 F-ISHOLIDAY      PIC X(20).
+
+       01 TOTAL-VENDAS     PIC 9(10)V99 VALUE 0.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -41,59 +43,89 @@
            DISPLAY "=== LENDO CSV ===".
 
            OPEN INPUT ARQ-CSV
+           OPEN OUTPUT ARQ-REL
 
+           MOVE "RELATÓRIO DETALHADO DAS VENDAS DE CADA SEMANA"
+               TO LINHA-REL
+           WRITE LINHA-REL
+
+           MOVE "---------------------------------------------"
+               TO LINHA-REL
+           WRITE LINHA-REL
+
+           MOVE SPACES TO LINHA-REL
+
+      ****************************************************************
+           *> PROCESSA CSV
+      ****************************************************************
            PERFORM UNTIL EOF-FLAG = "S"
 
                READ ARQ-CSV
-                   AT END
-                       MOVE "S" TO EOF-FLAG
+                   AT END MOVE "S" TO EOF-FLAG
                END-READ
 
                IF EOF-FLAG NOT = "S"
 
                    MOVE SPACES TO
-                       F-STORE F-DATE F-TEMPERATURE F-FUELPRICE
-                       F-MD1 F-MD2 F-MD3 F-MD4 F-MD5
-                       F-CPI F-UNEMP F-ISHOLIDAY
+                       F-STORE
+                       F-DEPT
+                       F-DATE
+                       F-W_SALES
+                       F-ISHOLIDAY
 
                    UNSTRING LINHA-CSV
                        DELIMITED BY ","
                        INTO F-STORE
+                            F-DEPT
                             F-DATE
-                            F-TEMPERATURE
-                            F-FUELPRICE
-                            F-MD1
-                            F-MD2
-                            F-MD3
-                            F-MD4
-                            F-MD5
-                            F-CPI
-                            F-UNEMP
+                            F-W_SALES
                             F-ISHOLIDAY
                    END-UNSTRING
 
-                   DISPLAY "Registro:"
-                   DISPLAY "  Store:       " F-STORE
-                   DISPLAY "  Date:        " F-DATE
-                   DISPLAY "  Temperature: " F-TEMPERATURE
-                   DISPLAY "  Fuel Price:  " F-FUELPRICE
-                   DISPLAY "  MD1:         " F-MD1
-                   DISPLAY "  MD2:         " F-MD2
-                   DISPLAY "  MD3:         " F-MD3
-                   DISPLAY "  MD4:         " F-MD4
-                   DISPLAY "  MD5:         " F-MD5
-                   DISPLAY "  CPI:         " F-CPI
-                   DISPLAY "  Unemployment:" F-UNEMP
-                   DISPLAY "  IsHoliday:   " F-ISHOLIDAY
-                   DISPLAY "-------------------------"
+                   IF F-W_SALES NOT = SPACES
+                       COMPUTE TOTAL-VENDAS =
+                           TOTAL-VENDAS + FUNCTION NUMVAL(F-W_SALES)
+                   END-IF
+
+                   STRING
+                       F-STORE      DELIMITED BY SIZE
+                       " "          DELIMITED BY SIZE
+                       F-DEPT       DELIMITED BY SIZE
+                       " "          DELIMITED BY SIZE
+                       F-DATE       DELIMITED BY SIZE
+                       " "          DELIMITED BY SIZE
+                       F-W_SALES    DELIMITED BY SIZE
+                       " "          DELIMITED BY SIZE
+                       F-ISHOLIDAY  DELIMITED BY SIZE
+                       INTO LINHA-REL
+                   END-STRING
+
+                   WRITE LINHA-REL
 
                END-IF
 
            END-PERFORM
 
-           CLOSE ARQ-CSV
+           MOVE "---------------------------------------------"
+               TO LINHA-REL
+           WRITE LINHA-REL
 
-           DISPLAY "=== FINALIZADO ===".
+           MOVE "TOTAL DE VENDAS: " TO LINHA-REL
+           STRING
+               "TOTAL DE VENDAS: "
+               FUNCTION NUMVAL-C (TOTAL-VENDAS)
+               DELIMITED BY SIZE
+               INTO LINHA-REL
+           END-STRING
+           WRITE LINHA-REL
+
+           MOVE "Programa: testeCSV" TO LINHA-REL
+           WRITE LINHA-REL
+
+           CLOSE ARQ-CSV
+           CLOSE ARQ-REL
+
+           DISPLAY "Relatório gerado com sucesso!"
            GOBACK.
 
        END PROGRAM testeCSV.
